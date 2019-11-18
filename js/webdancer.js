8b08328ed16310e0577f9cea59482f24b22161e2
Ozan Ürkmez <arkantos250@gmail.com>
2019-11-18 12:18:48 +0300

    Further canges on clef

--- js/webdancer.js
+++ js/webdancer.js
@@ -1,10 +1,12 @@
 
 var clefImage = $("#white-clef");
 var headerContentDiv = $("#header-content");
+var headerContentTextDiv = $("#header-content div");
 
 ArrangeSize();
 function ArrangeSize(){
 	headerContentDiv.width($(window).width() - clefImage.width());
+	headerContentTextDiv.css('line-height', headerContentDiv.height() + "px");
 }
 
 window.addEventListener("resize", ArrangeSize, true);
